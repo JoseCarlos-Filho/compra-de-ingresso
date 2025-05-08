bb88afd3f37824eb47a7e396d86de9099a7f1945
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-05-07 21:18:04 -0300

    feat: função efetuarCompra() recebe os inputs e faz as validações das entradas e checagem de ingressos disponíveis.

--- js/outra-solucao.js
+++ js/outra-solucao.js
@@ -1,4 +1,4 @@
-const {log} = console;
+const {log, error} = console;
 
 function comprar() {
     const tipoIngresso = document.getElementById("tipo-ingresso").value;
@@ -21,4 +21,35 @@ function comprar() {
         document.getElementById("qtd").value = "";
         return;
     }
+
+    log(`${qtd} ingresso(s) do tipo ${tipoIngresso}`);
+
+    efetuarCompra(tipoIngresso, qtd);
+}
+
+function efetuarCompra(tipoIngresso, qtd) {
+    const idElementoQtdaDisponivel = `qtd-${tipoIngresso}`;
+    const elementoQtdaDisponivel = document.getElementById(idElementoQtdaDisponivel);
+
+    if(!elementoQtdaDisponivel) {
+        alert(`Erro: Não foi possivel encontrar o contador de ingressos para o tipo ${tipoIngresso}`);
+        error(`Elemento com ID "${idElementoQtdaDisponivel}" não encontrado.`);
+        return;
+    }
+
+    const qtdaDisponivel = parseInt(elementoQtdaDisponivel.textContent);
+
+    if(isNaN(qtdaDisponivel)) {
+        alert(`Erro: A quantidade disponível para ingressos do tipo "${tipoIngresso}" não é um valor válido`);
+        error(`O conteúdo do elemento "${idElementoQtdaDisponivel}" não é um número: ${elementoQtdaDisponivel.textContent}`);
+        return;
+    }
+
+    log(`Disponível: (${tipoIngresso}): ${qtdaDisponivel}, Tentando comprar: ${qtd}`);
+
+    if(qtd > qtdaDisponivel) {
+        alert(`Quantidade indisponível para ingressos do tipo "${tipoIngresso}".`);
+        return;
+    }
+     
 }
